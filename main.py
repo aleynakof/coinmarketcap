f4c5d05f8212c2d56077a42a6bc6b91db219d9b6
aleynak-git <kofaleyna@gmail.com>
2021-03-06 11:31:41 +0300

    data is added with api

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import requests
 import json
+
+
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=9134ad4d-3ba1-4017-85b1-c5ec86a2a50a")
+print(api_request)
+# düzgün formatta alabilmek için
+result = json.loads(api_request.content)
+print(result)
